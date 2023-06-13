60959a69134b8bbb5f1ed789f23707ce3f9767df
yap cheah shen <yapcheahshen@gmail.com>
2023-06-13 22:16:07 +0800

    minor

--- swipegallery/dc/009.js
+++ swipegallery/dc/009.js
@@ -1,3 +1,3 @@
-jsonp(9,{"name":"dc","start":2778},`id
-;9=2=2=2=2=2
+jsonp(9,{"name":"dc","start":2784},`id
+;9=3=3=3=3=3
 vcpp_kumarajiva	vcpp_bodhiruci	vcpp_gupta	vcpp_paramartha	vcpp_xuanzang	vcpp_yijing`)
